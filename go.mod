5a45a680f2070ad051b75bfa3addd56fcbcdba30
Tanuj Mishra <tnj@Tanujs-MacBook-Pro.local>
2023-05-24 10:40:27 +0530

    Initial Commit

---
+++ go.mod
@@ -0,0 +1,14 @@
+module github.com/tanuj077/go-bookstore
+
+go 1.20
+
+require gorm.io/gorm v1.25.1
+
+require (
+	github.com/go-sql-driver/mysql v1.7.0 // indirect
+	github.com/gorilla/mux v1.8.0
+	github.com/jinzhu/gorm v1.9.16
+	github.com/jinzhu/inflection v1.0.0 // indirect
+	github.com/jinzhu/now v1.1.5 // indirect
+	gorm.io/driver/mysql v1.5.1
+)
---
+++ pkg/routes/bookstore-routes.go
@@ -0,0 +1,15 @@
+package routes
+
+import (
+	"github.com/gorilla/mux"
+	"github.com/tanuj077/go-bookstore/pkg/controllers"
+)
+
+var RegisterBookStoreRoutes = func (router *mux.Router)  {
+	router.HandleFunc("/book/", controllers.CreateBook).Methods("POST")
+	router.HandleFunc("/book/", controllers.GetAllBook).Methods("GET")
+	router.HandleFunc("/book/{bookId}", controllers.GetBookById).Methods("GET")
+	router.HandleFunc("/book/{bookId}", controllers.UpdateBook).Methods("PUT")
+	router.HandleFunc("/book/{bookId}", controllers.DeleteBook).Methods("DELETE")
+}
+
